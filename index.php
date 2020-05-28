8523933023b3e049a105274e0147454dda282ae7
zuigend <zuigend@gmail.com>
2020-05-28 21:57:34 +0900

    Add parameter

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         'requestTime' => $_SERVER["REQUEST_TIME"],
         'globalIpAddress' => $_SERVER["HTTP_X_FORWARDED_FOR"],
         'localIpAddress' => $_SERVER["REMOTE_ADDR"],
-        'hostIpAddress' => $_SERVER["REMOTE_HOST"]
+        'hostIpAddress' => $_SERVER["REMOTE_HOST"],
+        'hostIpAddress' => $_SERVER["REMOTE_USER"]
     );
     echo json_encode($parameters);
 ?>
