830603bbdcc8d180fde85f7bd1859e72523ccfce
zuigend <zuigend@gmail.com>
2020-05-28 21:58:24 +0900

    Fixed param

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         'globalIpAddress' => $_SERVER["HTTP_X_FORWARDED_FOR"],
         'localIpAddress' => $_SERVER["REMOTE_ADDR"],
         'hostIpAddress' => $_SERVER["REMOTE_HOST"],
-        'hostIpAddress' => $_SERVER["REMOTE_USER"]
+        'remoteUser' => $_SERVER["REMOTE_USER"]
     );
     echo json_encode($parameters);
 ?>
